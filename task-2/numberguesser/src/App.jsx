0f233024c486b2eb23a2615f6401e52329dfb293
notsamaltman <panwalkarsoham@gmail.com>
2025-08-04 01:35:31 +0530

    created initial layout of site

--- task-2/numberguesser/src/App.jsx
+++ task-2/numberguesser/src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-[rgb(24,24,24)] w-screen h-screen flex justify-around items-center flex-col'>
+      <div className="navbar w-screen h-[7%] bg-purple-900 fixed top-0 flex justify-center items-center">
+          <h1 className='text-white text-5xl'>NumberGuesser</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="lives-bar w-[45%] h-[10%] bg-[rgb(48,48,48)] -translate-y-70 flex justify-around items-center">
+          <div className='h-[80%] w-[8%] bg-red-500'></div>
+          <div className='h-[80%] w-[8%] bg-red-500'></div>
+          <div className='h-[80%] w-[8%] bg-red-500'></div>
+          <div className='h-[80%] w-[8%] bg-red-500'></div>
+          <div className='h-[80%] w-[8%] bg-red-500'></div>
+          <div className='h-[80%] w-[8%] bg-red-500'></div>
+          <div className='h-[80%] w-[8%] bg-red-500'></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
